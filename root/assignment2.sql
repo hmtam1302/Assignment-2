0b9fc32848452d22138c893e6ef9627c382e6c5a
hoangvutinh <hoangvutinh1@gmail.com>
2020-12-25 21:30:19 +0700

    add product.sql, modify product.php

---
+++ root/assignment2.sql
@@ -0,0 +1,34 @@
+drop schema  if exists assignment2;
+create schema assignment2;
+use assignment2;
+drop table if exists product;
+create table product(
+name varchar(59),
+author varchar(50),
+type varchar(50),
+url varchar(100)
+
+);
+
+insert INTO product(name,author,type,url) values("A Madness of Sunshine","Nalini singh","Novel","assets/img/work-1.jpg");
+insert INTO product(name,author,type,url) values("Novice Dragoneer","Django Weller","Novel","assets/img/work-2.jpg");
+insert INTO product(name,author,type,url) values("Me before you","Jojo Moyes","Novel","assets/img/work-3.jpg");
+insert INTO product(name,author,type,url) values("Forget You Know Me","Jessica Strawser","Novel","assets/img/work-4.jpg");
+insert INTO product(name,author,type,url) values("The best of me","Nicholas sparks","Novel","assets/img/work-5.jpg");
+insert INTO product(name,author,type,url) values("The wolf and the Watchman","Niklas Natt obc Dag","Novel","assets/img/work-6.jpg");
+insert INTO product(name,author,type,url) values("The Good Guy","Mark Mcallister","Novel","assets/img/work-7.jpg");
+insert INTO product(name,author,type,url) values("At the Going Down of the Sun","Marius Oelschig","Novel","assets/img/work-8.jpg");
+insert INTO product(name,author,type,url) values("The Imperfections of Memory","Angelina Aludo","Novel","assets/img/work-9.jpg");
+insert INTO product(name,author,type,url) values("The King of Drugs","Nora Barrett","Novel","assets/img/work-10.jpg");
+insert INTO product(name,author,type,url) values("Leviathan Loch","Mark Miller","Novel","assets/img/work-11.jpg");
+insert INTO product(name,author,type,url) values("Histories Cans","Fleurs","Comic","assets/img/work-12.jpg");
+insert INTO product(name,author,type,url) values("The cerulean","Amy Ewing","Novel","assets/img/work-13.jpg");
+insert INTO product(name,author,type,url) values("The book of Chaos","Jessica Renwick","Comic","assets/img/work-14.jpg");
+insert INTO product(name,author,type,url) values("A Daydreamer's Utopia","Louise Hunts","Novel","assets/img/work-15.jpg");
+insert INTO product(name,author,type,url) values("The priory of the Orange tree","Samantha Shannon","Novel","assets/img/work-16.jpg");
+insert INTO product(name,author,type,url) values("A game of Thrones","George R.R Martin","Novel","assets/img/work-17.jpg");
+insert INTO product(name,author,type,url) values("The edge of The Unknown","Andrew Hamburg","Novel","assets/img/work-18.jpg");
+insert INTO product(name,author,type,url) values("Dark Space","Author name","Novel","assets/img/work-19.jpg");
+insert INTO product(name,author,type,url) values("Harry Potter and the Philosopher's Stone","J.L.Rowling","Novel","assets/img/work-20.jpg");
+insert INTO product(name,author,type,url) values("Tess of the Road","Rachel Hartman","Novel","assets/img/work-21.jpg");
+
---
+++ root/products.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Boostrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!--Font-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!--Style file-->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="assets/js/main.js"></script>
+
+    <!--Animation.css-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+
+    <title>Price Page</title>
+</head>
+
+<body data-new-gr-c-s-check-loaded="8.867.0" id="pagebody">
+    <!--Header / Navbar-->
+    <nav id="mainNav" class="navbar navbar-expand-md fixed-top animate__animated animate__slideInDown">
+        <div class="container d-flex justify-content-between align-items-center flex-wrap">
+            <button class="login-btn btn btn-outline-primary" #id="login" href="login.html">Login</button>
+            <a href="index.html" class="navbar-brand font-weight-bold" id="projectName">Bookstore</a>
+            <button type="button" class="btn" data-toggle="collapse" data-target="#navbarDefault"><i
+                    class="material-icons" id="nav-icon">menu</i></button>
+            <div class="line-break"></div>
+            <div id="navbarDefault" class="navbar-collapse collapse justify-content-center align-items-center">
+                <ul class="nav navbar-nav text-uppercase font-weight-bold">
+                    <li class="nav-item">
+                        <a href="index.html" class="nav-link nav-link-hover">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="about.html" class="nav-link nav-link-hover">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="categories.html" class="nav-link nav-link-hover">Categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="products.html" class="nav-link nav-link-hover active-item">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="contact.html" class="nav-link nav-link-hover">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- ======= Products Section ======= -->
+    <section id="work" class="portfolio-mf sect-pt4 route">
+        <div class="container">
+            <div class="row  animate__animated animate__fadeInDown">
+                <div class="col-sm-12">
+                    <div class="price-box">
+                        <div class="title-box text-center">
+                            <h3 class="title-a">
+                                Price Details
+                            </h3>
+                            <p class="subtitle-a">
+                                Products
+                            </p>
+                            <div class="line-mf"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- ======= Get Url Image from Database ======= -->
+            <div class="row">
+            <?php
+            define('DB_HOST', 'localhost'); 
+            define('DB_NAME', 'assignment2'); 
+            define('DB_USER','root'); 
+            define('DB_PASSWORD',''); 
+            
+            $con=mysqli_connect(DB_HOST,DB_USER,'',DB_NAME) ; 
+            
+            $database="select * from product";
+            $record=mysqli_query($con,$database);
+            while ($row =mysqli_fetch_assoc($record))
+            {  $path= $row['url'];
+                $name=$row['name'];
+                $author=$row['author'];
+                $type=$row['type'];
+            ?>
+           
+          
+          
+                <div class="col-md-4 ">
+                    <div class="work-box animate__animated animate__fadeInLeftBig">
+                        <a href="" data-gall="portfolioGallery" class="venobox">
+                            <div class="work-img">
+                            <?php echo '<img src="'.$path.'" alt="" class="img-fluid">' ?>
+                         
+                            </div>
+                        </a>
+                        <div class="work-content">
+                            <div class="row">
+                                <div class="col-sm-8">
+                                    <h2 class="w-title"><?php echo $name ?></h2>
+                                    <p>Author: <?php echo $author ?></p>
+                                    <p>Description: <?php echo $type ?></p>
+                                    <div class="w-more">
+                                        <span class="w-ctegory">Price</span> - <span class="w-date">15$</span>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="w-like">
+                                        <a href="portfolio-details.html"> <span class="ion-ios-plus-outline"></span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+            <?php
+            }
+            ?>
+               
+                              
+            </div>
+        </div>
+    </section>
+    <!-- End Portfolio Section -->
+    <!--Back to to-->
+    <a href="#page-body" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+    <!--Preloader-->
+    <div id="preloader"></div>
+</body>
